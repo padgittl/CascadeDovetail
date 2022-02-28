32438f0ae1a55a33624268ff7a4cf5d0401fc198
padgittl <padgittl@oregonstate.edu>
2022-02-28 13:28:35 -0800

    update

---
+++ TimeDivergenceEstimation/TimeDivergenceEstimation_copyFiles.sh
@@ -0,0 +1,2 @@
+cp /nfs0/BB/Hendrix_Lab/Hops/DovetailAnalysis/OrthoFinder/runDirs/minLength100aa/singleCopyOrthologAnalysis/transdecoderMakerCombined/clock/SpeciesTree_rooted.run05.txt .
+cp /nfs0/BB/Hendrix_Lab/Hops/DovetailAnalysis/OrthoFinder/runDirs/minLength100aa/singleCopyOrthologAnalysis/transdecoderMakerCombined/clock/run05_01062022/thirdPos/clock3/samplefreq500/mcmctree.ctl .
